b9c1e3d97ec39df84541c59400419f3eaeea635f
JustAnotherGalaxyGuy <139143722+ThatRandomCoder1@users.noreply.github.com>
2026-06-08 17:15:25 +0300

    Working web connection :)
    Works by entering the WiFi SSID and password, outputting a local IP with a webpage saying "Hello World!" (and a smaller one)

--- fanthing.ino
+++ fanthing.ino
@@ -1,42 +1,60 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 #include "DHT.h"
 
 #define tempSensor D7
 #define sensorType DHT11
 
+ESP8266WebServer server(80);
 DHT dht(tempSensor, sensorType);
 
 void setup() {
-  // put your setup code here, to run once:
-Serial.begin(115200);
-Serial.println("ready!");
-dht.begin();
+  Serial.begin(115200);
+  Serial.println("ready!");
 
-analogWriteFreq(1000);
-pinMode(D1, OUTPUT);
+  dht.begin();
+
+  analogWriteFreq(1000);
+  pinMode(D1, OUTPUT);
+
+  WiFi.begin("wifiname", "wifipassword");
+  while (WiFi.status() != WL_CONNECTED){
+    delay(1000);
+    Serial.print("wait..");
+  }
+  Serial.println("Connected!");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", []() {
+    server.send(200, "text/html", "<h1>Hello World!</h1><h2>Hello World, but smaller!</h2>");
+  });
+
+  server.begin();
+  Serial.println("well, that was a success.");
 }
 
 void loop() {
 
-// reading for the temp sensor
-
-/*
- float t = dht.readTemperature();
+//reading for the temp sensor
+//  float t = dht.readTemperature();
  
-  if (isnan(t)) {
-    Serial.println("haha fail");
-    return;
-  }
+//   if (isnan(t)) {
+//     Serial.println("haha fail");
+//     return;
+//   }
   
-  Serial.print("Temp: ");
-  Serial.print(t);
-*/
+//   Serial.print("The temperature is... ");
+//   Serial.print(t);
 
-// stepper driver test (used stepper driver pcb due to not having the parts for normal fan pwm from esp8266)
 
-analogWrite(D1, 128);
-delay(3000);
-analogWrite(D1, 0);
-delay(3000);
+// // Placeholder for motor
+// analogWrite(D1, 128);
+// delay(3000);
+// analogWrite(D1, 0);
+// delay(3000);
+
+//temporary thing for webpage
+  server.handleClient();
 
 
 }
